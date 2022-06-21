7cb4283cf543b56f7ce18aec4c62d51c719114db
Michael Steenbeek <m.o.steenbeek@gmail.com>
2022-06-21 21:26:26 +0200

    Fix #1: IND files from A2 Racer II and III cannot be extracted

--- rcs-extract.php
+++ rcs-extract.php
@@ -19,7 +19,15 @@ $numFiles = unpack('v', fread($indexFileHandle, 2))[1];
 
 for ($i = 0; $i < $numFiles; $i++)
 {
-    $filename = trim(utf8_encode(fread($indexFileHandle, 20)));
+    $rawFilename = fread($indexFileHandle, 20);
+    // C stops reading when it encounters a null character, even if there are stray characters after it.
+    // We need to do this as well, because the files of A2 Racer II and III contain garbage characters after the null.
+    $nullPos = strpos($rawFilename, "\x00");
+    if ($nullPos !== false) {
+        $rawFilename = substr($rawFilename, 0, $nullPos);
+    }
+
+    $filename = trim(utf8_encode($rawFilename));
     $offset = unpack('V', fread($indexFileHandle, 4))[1];
 
     printf('File %d: "%s", offset %d' . PHP_EOL, $i, $filename, $offset);
